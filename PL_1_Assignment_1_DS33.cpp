8fd3e42ce0c80c404f9c3772b1e14859a0b9aecf
surilymane <88475581+surilymane@users.noreply.github.com>
2025-08-04 15:47:50 +0530

    Add files via upload

---
+++ PL_1_Assignment_1_DS33.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+typedef struct date
+{
+  int day, month, year;
+}date;
+
+typedef struct book
+{
+  string name;
+  int book_id;
+  date pub_date;
+}book;
+
+int main()
+{
+  book books[n];
+  int choice;
+  
+  cout<<"Please enter 1 to add books, 2 to search a book, 3 to view list of books: "<<endl;
+  cin>>choice;
+  
+  switch(choice)
+  {
+  
+    int n;
+  
+    cout<<"Please enter the number of books: "<<endl;
+    cin>>n;
+  
+    case 1:
+    {
+      for(int i = 0; i < n; i++)
+      {
+        cout<<"Please enter the book name and id of book"<<i+1<<": "<<endl;
+        cin>>books[i].name>>books[i].book_id;
+    
+        cout<<"Please enter the date of publishing in dd mm yyyy format: "<<endl;
+        cin>>books[i].pub_date.day>>books[i].pub_date.month>>books[i].pub_date.year;
+      }
+      break;
+    }
+    
+    case 2:
+    {
+      int id;
+  
+      cout<<"Please enter a book ID to view details: "<<endl;
+      cin>>id;
+    
+      int count = 0;
+  
+      for(int j = 0; j < n; j++)
+      {
+        if(id == books[j].book_id)
+        {
+          cout<<"The details of the book are:"<<endl<<"Name: "<<books[j].name<<endl<<"ID: "<<books[j].book_id<<endl<<"Date of Publishing: "<<books[j].pub_date.day<<"/"<<books[j].pub_date.month<<"/"<<books[j].pub_date.year<<endl;
+      
+          count++;
+      
+          break;
+        }
+      }
+  
+      if(count == 0)
+      {
+        cout<<"Invalid ID."<<endl;
+      }
+    }
+    
+    /*case 3:
+    {
+      */
+    
+    default:
+    {
+      cout<<"Invalid choice.";
+      
+      break;
+    }
+  
+  return 0;
+}
+  
+  
+  
