687a309b0bbc621e01c84b1ab0566e410aec7e24
surilymane <88475581+surilymane@users.noreply.github.com>
2025-10-30 12:07:23 +0530

    Update DS30_Insertion_Sort.c

--- DS30_Insertion_Sort.c
+++ DS30_Insertion_Sort.c
@@ -3,6 +3,33 @@
 
 int insertion_sort(int array[], int n)
 {
+	int comp_count = 0;
+	int push = 0;
+	
+    for(int k = 0; k < n; k++)
+    {
+        int t = array[k];
+        int ptr = k - 1;
+        
+        while(t < array[ptr])
+        {
+            array[ptr+1] = array[ptr];
+            ptr--;
+        }
+        comp_count++;
+        
+        array[ptr+1] = t;
+        push++;
+    }
+    
+    printf("\nSorted array: \n");
+    
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", array[i]);
+    }
+    
+    printf("\nNumber of Pushes: %d\nNumber of Comparisons: %d\n.", push, comp_count);
     return 1;
 }
 
@@ -14,6 +41,12 @@ int main()
     for(int i = 0; i < n; i++)
     {
         array[i] = rand() % 51;
+    }
+    
+    printf("\nRandomly generated array: \n");
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", array[i]);
     }
 
     insertion_sort(array, n);
